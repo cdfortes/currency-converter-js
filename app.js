bcc7adf777b2196cc085e21a74dca6da549f2d76
cdfortes <cdfortes@gmail.com>
2021-07-29 00:16:59 -0100

    initial configs

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
 const APIKey = 'e1d32ce84c794bb1a604ca06'
+const baseURl = `https://v6.exchangerate-api.com/v6/${APIKey}/`
 
-const url = `https://v6.exchangerate-api.com/v6/${APIKey}/latest/USD`
+const currencyOne = document.querySelector('[data-js="currency-one"]')
+const currencyTwo = document.querySelector('[data-js="currency-two"]')
+const convertedValue = document.querySelector('[data-js="converted-value"]')
+const currencyOneTimes = document.querySelector(
+  '[data-js="currency-one-times"]'
+)
 
-fetch(url).then((item) => console.log(item.json().value))
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error('Não foi possivel obter os dados')
+    }
+
+    return response.json()
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`)
+  }
+}
+
+const currenciesCodes = async () => {
+  const url = `${baseURl}latest/USD`
+  const { conversion_rates } = await fetchData(url)
+
+  const codes = Object.keys(conversion_rates)
+  codes.forEach((currencyCode) => {
+    currencyOne.innerHTML += `<option value="${currencyCode}">${currencyCode}</option>`
+  })
+  codes.reverse().forEach((currencyCode) => {
+    currencyTwo.innerHTML += `<option value="${currencyCode}">${currencyCode}</option>`
+  })
+}
+currenciesCodes()
+
+
+convertedValue.textContent = `5.0615`
+currencyOneTimes.value = '1'
+
+const getExchangeRate = async (event) => {
+  const baseCode = currencyOne.value
+  const targetCode = currencyTwo.value
+  const amount = event.target.value
+
+  const url = `${baseURl}pair/${baseCode}/${targetCode}/${amount}`
+
+  const { conversion_result } = await fetchData(url)
+
+  convertedValue.textContent = conversion_result
+}
+currencyOneTimes.addEventListener('input', getExchangeRate)
